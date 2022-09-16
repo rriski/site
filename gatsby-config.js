0d8e5104b0acfcbe6a4b74c37f99edd45eb31dfd
Timo Riski <25483483+rriski@users.noreply.github.com>
2022-09-16 13:54:48 +0300

    chore: remove `gatsby-plugin-react-svg`

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,13 +44,5 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /images/,
-        },
-      },
-    },
   ],
 }
